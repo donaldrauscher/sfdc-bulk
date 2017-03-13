892f1dbc642c18726f5b1f4a182a366664496a84
Donald Rauscher <donald.rauscher@gmail.com>
2017-03-13 08:01:09 -0500

    Remove download link from GH

--- setup.py
+++ setup.py
@@ -3,10 +3,9 @@ from setuptools import setup
 setup(
     name='sfdc-bulk',
     packages=['sfdc_bulk'],
-    version='0.1',
+    version='0.2',
     description='Python client library for SFDC bulk API',
     url='https://github.com/donaldrauscher/sfdc-bulk',
-    download_url='https://github.com/donaldrauscher/sfdc-bulk/archive/0.1.tar.gz',
     author='Donald Rauscher',
     author_email='donald.rauscher@gmail.com',
     license='MIT',
